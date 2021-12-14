73fc5e3bd5375765b4ce7e7d3f694274894d31c5
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-12-14 20:00:38 -0300

    add configuração básica de roteamento e estilos

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello World2</h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Home } from './pages/home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
