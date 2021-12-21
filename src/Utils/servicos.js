e87f9c0205bd6e11ff115fe23114b370362af33a
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-12-21 17:14:41 -0300

    add serviçoes estáticos

---
+++ src/Utils/servicos.js
@@ -0,0 +1,28 @@
+const servicos = [
+  {
+    nome: 'Máquina',
+    valor: '20,00',
+    imagem: 'https://i.imgur.com/lIEkxN4.png',
+    selecionado: true,
+  },
+  {
+    nome: 'Tesoura',
+    valor: '15,00',
+    imagem: 'https://i.imgur.com/1Ken3cC.png',
+    selecionado: true,
+  },
+  {
+    nome: 'Navalha',
+    valor: '5,00',
+    imagem: 'https://i.imgur.com/ZNOdJdq.png',
+    selecionado: false,
+  },
+  {
+    nome: 'Barba',
+    valor: '8,00',
+    imagem: 'https://i.imgur.com/QYISrOT.png',
+    selecionado: false,
+  },
+];
+
+export { servicos };
