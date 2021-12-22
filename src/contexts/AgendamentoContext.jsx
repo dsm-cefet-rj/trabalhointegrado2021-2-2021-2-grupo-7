2628547b3bc09dce37c1977039a25a18f2146162
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-12-22 19:40:53 -0300

    add estado do cliente ao contexto

--- src/contexts/AgendamentoContext.jsx
+++ src/contexts/AgendamentoContext.jsx
@@ -1,10 +1,5 @@
 import { useState, createContext } from 'react';
 
-// export type HomeContextProps = {
-//   isHomePage: boolean;
-//   setIsHomePage: Dispatch<SetStateAction<boolean>>;
-// };
-
 export const AgendamentoContext = createContext({});
 
 export function AgendamentoContextProvider(props) {
@@ -13,14 +8,23 @@ export function AgendamentoContextProvider(props) {
   const [horarioSelecionado, setHorarioSelecionado] = useState(null);
   const [servicoSelecionado, setServicoSelecionado] = useState(null);
   const [funcionarioSelecionado, setFuncSelecionado] = useState(null);
-  // const [isHomePage, setIsHomePage] = useState(true);
-  // const [isHomePage, setIsHomePage] = useState(true);
-  // const [isHomePage, setIsHomePage] = useState(true);
-  // const [isHomePage, setIsHomePage] = useState(true);
+  const [cliente, setCliente] = useState(null);
 
   return (
     <AgendamentoContext.Provider
-      value={{ horarioSelecionado, setHorarioSelecionado, servicoSelecionado , setServicoSelecionado, funcionarioSelecionado, setFuncSelecionado }}
+      value={{
+        horarioSelecionado,
+        setHorarioSelecionado,
+
+        servicoSelecionado,
+        setServicoSelecionado,
+
+        funcionarioSelecionado,
+        setFuncSelecionado,
+
+        cliente,
+        setCliente,
+      }}
     >
       {children}
     </AgendamentoContext.Provider>
