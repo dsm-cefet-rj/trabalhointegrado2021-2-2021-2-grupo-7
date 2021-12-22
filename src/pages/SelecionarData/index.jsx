47f0b03c69dd77f69385c97d47ed51564b153357
juwtf <90166970+juwtf@users.noreply.github.com>
2021-12-21 22:25:50 -0300

    att link do botao proximo da tela de data

--- src/pages/SelecionarData/index.jsx
+++ src/pages/SelecionarData/index.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import { Header } from '../../components/Header';
 
 import rightArrowIcon from '../../assets/right-arrow.svg';
