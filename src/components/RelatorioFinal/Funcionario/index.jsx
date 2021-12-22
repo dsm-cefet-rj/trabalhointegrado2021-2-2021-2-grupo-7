8415dff3f523cc59b740c42be05862a0a1568a57
juwtf <90166970+juwtf@users.noreply.github.com>
2021-12-21 23:16:11 -0300

    att foto funcionario e add especialidade

--- src/components/RelatorioFinal/Funcionario/index.jsx
+++ src/components/RelatorioFinal/Funcionario/index.jsx
@@ -1,14 +1,16 @@
-import fotoFuncionario from '../../../assets/profile.png';
+import fotoFuncionario from '../../../assets/barbeiro2.png';
 
 import styles from './styles.module.css';
 
 function Funcionario(props) {
-  const { nome } = props;
+  const { nome, especialidade } = props;
+
 
   return (
     <div className={styles.infoFuncionario}>
       <img src={fotoFuncionario} alt="foto funcionario" />
       <strong>{nome}</strong>
+      <strong>{especialidade}</strong>
     </div>
   );
 }
