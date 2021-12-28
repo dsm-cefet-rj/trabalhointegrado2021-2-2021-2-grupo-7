6dee218362b8dd78a3da31b89dbaa466c39c0910
JoaoPedroNC <95342464+JoaoPedroNC@users.noreply.github.com>
2021-12-28 15:09:41 -0300

    Update Identificar-cliente.js

--- Identificar-cliente.js
+++ Identificar-cliente.js
@@ -7,6 +7,31 @@
 link rel = "stylesheet" href = "css/style.css">
 </head>
 <body>
+  <div id= "main-container">
+    <h1>Cadastre-se para acessar a barbearia</h1>
+<form id = "register form"
+<div class = "full-box">
+  <label for "email">E-mail</label>
+<input type= "email"name="email"id="email"placeholder="Digite seu e-mail">
+  </div>
+<div class = "half-box">
+  <label for"name">Nome</label>
+<input type="text" name= "name" id="name"placeholder="Digite o seu nome">
+  </div>
+<div class = "half-box">
+  <label for"lastname">Sobrenome</label>
+<input type="text" name= "lastname" id="lastname"placeholder="Digite o seu sobrenome">
+  </div>
+div class = "half-box">
+  <label for"password">Senha</label>
+<input type="password" name= "password" id="password"placeholder="Digite a sua senha">
+  </div>
+div class = "half-box">
+  <label for"passConfirmation">Sobrenome</label>
+<input type="password" name= "password" id="passConfirmation"placeholder="Digite a sua senha">
+  </div>
+</form> 
+    </div>
   <script src= "js/script.js"></script>
 </body>
 </html>
