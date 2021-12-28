1f353ea5d15fa906262bb4ebcaa658c3b4775e47
JoaoPedroNC <95342464+JoaoPedroNC@users.noreply.github.com>
2021-12-28 14:40:37 -0300

    Create Identificar-cliente.js

---
+++ Identificar-cliente.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html!>
+  <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+      <meta name= "viewport" content="widthdevice-width,initial-scale=1.0">
+        <title>Registro de Cliente</title>
+link rel = "stylesheet" href = "css/style.css">
+</head>
+<body>
+  <script src= "js/script.js"></script>
+</body>
+</html>
