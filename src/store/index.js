6bf84d7c1b718878311a03e582addb17d4191a4e
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-12-29 01:01:23 -0300

    refatorando redux cfg

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers } from 'redux';
+import { clienteReducer } from './ClienteAgendamento/Cliente.reducer';
 import { dataReducer } from './DataAgendamento/Data.reducer';
 import { funcionarioReducer } from './FuncionarioAgendamento/Funcionario.reducer';
 import { horarioReducer } from './HorarioAgendamento/Horario.reducer';
@@ -9,6 +10,7 @@ const rootReducer = combineReducers({
   horario: horarioReducer,
   funcionario: funcionarioReducer,
   servicos: servicoReducer,
+  cliente: clienteReducer,
 });
 
 const store = createStore(rootReducer);
