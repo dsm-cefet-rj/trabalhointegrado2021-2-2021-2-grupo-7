09c6b3e1777792fd8e9e41c6d001dd315e026641
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-12-29 00:57:56 -0300

    refatoração do redux e removendo arquivos de mock

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers } from 'redux';
 import { dataReducer } from './DataAgendamento/Data.reducer';
+import { funcionarioReducer } from './FuncionarioAgendamento/Funcionario.reducer';
 import { horarioReducer } from './HorarioAgendamento/Horario.reducer';
+import { servicoReducer } from './ServicosAgendamento/Servico.reducer';
 
 const rootReducer = combineReducers({
   data: dataReducer,
   horario: horarioReducer,
+  funcionario: funcionarioReducer,
+  servicos: servicoReducer,
 });
 
 const store = createStore(rootReducer);
